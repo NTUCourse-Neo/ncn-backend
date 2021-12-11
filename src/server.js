8e34295c67fdcd8cadbd1f95e053a1cf76b528a7
jc-hiroto <taste.ful.msa@gmail.com>
2021-12-11 11:54:36 +0800

    Moved mongoclient and search function to new files and update dependencies.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import { MongoClient } from 'mongodb';
 import express from 'express';
-import router from './route/index';
+import router from './routes/index';
+import course_router from './routes/courses';
 import cors from 'cors';
 require('dotenv-defaults').config();
 
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1', router);
+app.use('/api/v1/courses', course_router);
+
+
 
 app.listen('5000', async () => {
   console.log('Server is running on port 5000');
