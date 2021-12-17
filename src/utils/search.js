3071d3b918a3e966f1b3362349e6ebdbed2d75e3
jc-hiroto <taste.ful.msa@gmail.com>
2021-12-17 21:43:52 +0800

    Modified search method response content.

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,7 +1,7 @@
 async function search(query, paths = ['course_name', 'teacher'], collection) {
   try {
     console.log('Start searching...');
-    let result = await collection
+    let objArray = await collection
       .aggregate([
         {
           $search: {
@@ -13,9 +13,10 @@ async function search(query, paths = ['course_name', 'teacher'], collection) {
           },
         },
         { $limit: 20 },
-        { $project: { _id: 1, id: 1, department: 1, course_code: 1, class_id: 1, course_name: 1, credit: 1, course_id: 1, is_half_year: 1, required: 1, teacher: 1, enroll_method: 1, time_loc: 1, total_slot: 1, limit: 1, note: 1, url: 1, semester: 1, departments_dependency: 1, is_intensive_course: 1, time_loc_pair: 1, provider: 1 } },
+        { $project: { _id: 1 } },
       ])
       .toArray();
+    let result = objArray.map(a => a._id);
     return result;
   } catch (err) {
     console.log(err);
