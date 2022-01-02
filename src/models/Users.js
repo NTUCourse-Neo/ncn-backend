15f553821a05f5f5c4a897a14b4ae4c0406850b7
jc-hiroto <taste.ful.msa@gmail.com>
2022-01-02 14:53:52 +0800

    Added user get post patch methods.

--- src/models/Users.js
+++ src/models/Users.js
@@ -3,13 +3,11 @@ const users = new mongoose.Schema({
     _id: String,    // 流水號
     name: String,    // 姓名
     email: String,   // email
-    gender: Number,  // 性別(可能不用?)
     student_id: String,  // 學號
     department: Array,  // 學院
     minor: Array,   // 輔系
     year: Number,    // 年級
     language: String,    // 母語
-    country: String,     // 國籍
     favorites: Array, // 最愛課程列表(內為課程id)
     course_tables: Array,     // 課表列
     history_courses: Array,    // 已修過的課程
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,12 +5,26 @@ import * as auth0_client from "../utils/auth0_client";
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  let user_id = req.params.id;
-  let result;
+router.get('/:id', async (req, res) => {
+  const user_id = "auth0|"+req.params.id;
+  let auth0_user;
+  await auth0_client.get_token().then(async (token) => {
+    await auth0_client.get_user_by_id(user_id, token).then(async (user) => {
+      if (!user){
+        res.status(404).send({user: null, message: "User is not registered in Auth0"});
+        return;
+      }else{
+        auth0_user = user;
+      }
+    });
+  });
   try {
-    result = await Users.findOne({'_id': user_id});
-    res.status(200).send({user: result, message: "Get user"});
+    let result = await Users.findOne({'_id': user_id});
+    if(result){
+      res.status(200).send({user: result, auth0: auth0_user, message: "Successfully get user by id."});
+    }else{
+      res.status(404).send({user: null, message: "User not found in MongoDB."});
+    }
   }
   catch (err) {
       res.status(500).send({user: null, message: err});
@@ -23,25 +37,102 @@ router.post('/register', async (req, res) => {
   try{
     if(!email){
       res.status(400).send({message: "email is required"});
+      return;
     }else{
+      const db_user = await Users.findOne({'email': email});
+      if(db_user){
+        res.status(400).send({message: "email is already registered"});
+        return;
+      }
       await auth0_client.get_token().then(async (token) => {
         await auth0_client.get_user_by_email(email, token).then(async (users) => {
           let user;
           if(users.length === 0){
             res.status(400).send({message: "email is not registered"});
+            return;
           }else if(users.length === 1){
             user = users[0];
           }else{
             user = users.filter(user => !user.identities.isSocial)[0];
           }
-          console.log(user);
+          let new_user = new Users({
+            _id: user.user_id,
+            name: user.name,
+            email: user.email,
+            gender: 0,
+            student_id: "",
+            department: [],
+            minor: [],
+            year: 0,
+            language: "",
+            favorites:[],
+            course_tables: [],
+            history_courses: []
+          });
+          new_user.save().then(() => {
+            res.status(200).send({message: "User created", user: new_user});
+            return;
+          }
+          ).catch((err) => {
+            res.status(500).send({message: err});
+            console.error(err);
+            return;
+          });
         }).catch((err) => {
+          res.status(500).send({message: err});
           console.error(err);
+          return;
         });
       })
     }
   }catch(err){
     res.status(500).send({user: null, message: err});
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+  const user_id = "auth0|"+req.params.id;
+  const patch_user = req.body.user;
+  // Check if user exists
+  try {
+    const user = await Users.findOne({'_id': user_id}).exec();
+    if(!user){
+      res.status(404).send({message: "User not found"});
+      return;
+    }
+    // Check if user is registered in Auth0
+    await auth0_client.get_token().then(async (token) => {
+      await auth0_client.get_user_by_id(user_id, token).then(async (user) => {
+        if (!user){
+          res.status(404).send({message: "User is not registered in Auth0"});
+          return;
+        }
+      });
+    });
+    // Update user
+    // Check each field and update if necessary.
+    let query = {};
+    for(let key in patch_user){
+      console.log(key);
+      if(key == "_id" || key == "email"){
+        res.status(400).send({message: "Cannot update _id or email"});
+        return;
+      }
+      if(key in user && (user[key] != patch_user[key])){
+        query[key] = patch_user[key];
+      }
+    }
+    if(Object.keys(query).length === 0){
+      res.status(200).send({message: "No update"});
+      return;
+    }
+    await Users.updateOne({'_id': user_id}, query).exec().then(() => {
+      res.status(200).send({updates: query, message: "User updated."});
+      return;
+    });
+  } catch (err) {
+    res.status(500).send({message: err});
+    console.error(err);
   }
 });
 
