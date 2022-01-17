3c452d1a4800e3cfba9515ab232e5a567f81e2d0
jc-hiroto <taste.ful.msa@gmail.com>
2022-01-18 00:32:25 +0800

    Update logging function.

--- src/routes/logs.js
+++ src/routes/logs.js
@@ -7,15 +7,12 @@ const router = express.Router();
 
 router.post('/error', async(req, res) => {
   try{
-    const err_comp = req.body.component;
-    const err_log = req.body.log;
-    const fields = [
-      {name: "Component", value: err_comp},
-      {name: "Error Log", value: "```\n" + err_log + "\n```"},
-      {name: "Request body", value: "```\n" + JSON.stringify(req.body) + "\n```"}
-    ]
+    // use request body msg as fields.
+    const fields = Object.keys(req.body).map(key => {
+      return {name: key, value: req.body[key]}
+    })
     console.log(fields)
-    await sendWebhookMessage("error","New error reported to logging api", fields);
+    await sendWebhookMessage("error","New error reported to logging api.", fields);
     res.status(200).send({message: 'Successfully reported error.'})
   }catch(err){
     console.error(err);
@@ -23,16 +20,15 @@ router.post('/error', async(req, res) => {
 });
 
 router.post('/info', async(req, res) => {
-  const err_comp = req.body.component;
-  const err_log = req.body.log;
-  const fields = [
-    {name: "Component", value: err_comp},
-    {name: "Info Log", value: "```\n" + err_log + "\n```"},
-    {name: "Request body", value: "```\n" + JSON.stringify(req.body) + "\n```"}
-  ]
-
-  await sendWebhookMessage("info","New info reported to logging api", fields);
-  res.status(200).send({message: 'Successfully reported info.'})
+  try{
+    const fields = Object.keys(req.body).map(key => {
+      return {name: key, value: req.body[key]}
+    })
+    await sendWebhookMessage("info","New info reported to logging api.", fields);
+    res.status(200).send({message: 'Successfully reported info.'})
+  }catch(err){
+    console.error(err);
+  }
 });
 
 
